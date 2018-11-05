9862bbe35b20ac6f265e7abed6fbaa51baedaa2d
BenjiRS94 <36006206+BenjiRS94@users.noreply.github.com>
2018-11-05 17:37:56 +0100

    testTransfer to TestAccountService

--- src/test/java/org/udg/caes/account/TestAccountService.java
+++ src/test/java/org/udg/caes/account/TestAccountService.java
@@ -1,20 +1,38 @@
 package org.udg.caes.account;
 
-import mockit.Expectations;
-import mockit.Verifications;
+import mockit.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import mockit.Tested;
-import mockit.Mocked;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestAccountService {
 
+  @Injectable AccountManager am;
+  @Tested AccountService as;
+
+  @BeforeEach
+  void setup() {
+    as = new AccountService();
+    as.setAccountManager(am);
+  }
 
   @Test
-  void testTransfer(@Tested AccountService as, @Mocked AccountManager am)  {
-    throw new UnsupportedOperationException("Not implemented yet");
+  void testTransfer(@Injectable Account a1, @Injectable Account a2)  {
+
+    new Expectations() {{
+      am.findAccount("id1"); result = a1;
+      am.findAccount("id2"); result = a2;
+    }};
+
+    as.transfer("id1", "id2", 50);
+
+    new Verifications() {{
+      a1.debit(50); times = 1;
+      a2.credit(50); times = 1;
+      am.updateAccount(a1); times = 1;
+      am.updateAccount(a2); times = 1;
+    }};
   }
 }
